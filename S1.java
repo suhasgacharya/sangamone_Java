72f7f51217d107f2b1881a73410083be7de4b3b7
Suhas G <147820454+suhasgacharya@users.noreply.github.com>
2023-10-31 15:32:08 +0530

    Update S1.java

--- S1.java
+++ S1.java
@@ -1,10 +1,25 @@
 package com.sangamone;
 
-public class S1 {
+public class S3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int a;
+		int b;
+		int c;
+		
+		for (a=3;a<=20;a++) {
+			for( b=1;b<=10;b++) {
+				   // System.out.println(b);
+				    c=a*b;
+				    System.out.println(a + "*" +b+ "=" +c);
+				    
+				}
+				System.out.println();
 
+		}
+		
+	
 	}
 
 }
